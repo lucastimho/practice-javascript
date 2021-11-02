b0985e017c8451cda6ea258fbb392b9a4b6091b9
lucastimho <lucas.t.ho.lh@gmail.com>
2021-11-02 12:44:57 -0500

    add highchart

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let chart; // globally available
-    document.addEventListener('DOMContentLoaded', function() {
-        chart = Highcharts.stockChart('container', {
-            rangeSelector: {
-                selected: 1
-            },
-            series: [{
-                name: 'USD to EUR',
-                data: usdtoeur // predefined JavaScript array
-           }]
-       });
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  chart = Highcharts.stockChart("container", {
+    rangeSelector: {
+      selected: 1,
+    },
+    series: [
+      {
+        name: "USD to EUR",
+        data: usdtoeur, // predefined JavaScript array
+      },
+    ],
+  });
+});
